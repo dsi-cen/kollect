564b270234cf18f51c904e48067fcdc0b888d482
Rémy Lebrun <39341330+rylebrun@users.noreply.github.com>
2019-03-26 16:22:39 +0100

    Gestion des départements fantômes

--- modeles/ajax/bilan/listedep.php
+++ modeles/ajax/bilan/listedep.php
@@ -6,7 +6,7 @@ function departement() // Liste des département de l'emprise
 {
     $bdd = PDO2::getInstance();
     $bdd->query("SET NAMES 'UTF8'");
-    $req = $bdd->query("SELECT iddep AS id, departement AS emp, poly, geojson FROM referentiel.departement ");
+    $req = $bdd->query("SELECT iddep AS id, departement AS emp, poly, geojson FROM referentiel.departement WHERE geojson IS NOT NULL ");
     $commune = $req->fetchAll(PDO::FETCH_ASSOC);
     $req->closeCursor();
     return $commune;
